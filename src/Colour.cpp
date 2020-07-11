618d48136436811e9142ef8f77ecccd8f3a9b3d2
sc15i2gp <sc15i2gp@leeds.ac.uk>
2020-07-11 05:29:27 +0100

    Added colour data functions.

--- src/Colour.cpp
+++ src/Colour.cpp
@@ -143,3 +143,230 @@ Spectrum generate_black_body_spd(double temperature, double start_wavelength, do
 	spd.end_wavelength = end_wavelength;
 	return spd;
 }
+
+char* find_next_line(char* c)
+{
+	for(; *c != '\n' && *c != 0; ++c);
+	++c;
+	return c;
+}
+
+char* find_next_character(char* c, char character)
+{
+	for(; *c != character && *c != 0; ++c);
+	return c;
+}
+
+bool is_number_character(char c)
+{
+	return (c >= '0' && c <= '9') || c == '-';
+}
+
+char* find_next_number(char* c)
+{
+	for(; !is_number_character(*c) && *c != 0; ++c);
+	return c;
+}
+
+Spectrum reference_white = {};
+Spectrum colour_matching_functions[3] = {};
+
+Spectrum white_rgb_to_spd = {};
+Spectrum cyan_rgb_to_spd = {};
+Spectrum magenta_rgb_to_spd = {};
+Spectrum yellow_rgb_to_spd = {};
+Spectrum red_rgb_to_spd = {};
+Spectrum green_rgb_to_spd = {};
+Spectrum blue_rgb_to_spd = {};
+
+Spectrum load_spd(const char* spd_path)
+{
+	Spectrum spd = {};
+	char* spd_contents = read_file_contents(spd_path);
+	
+	//Count number of samples
+	int number_of_samples = 0;
+	for(char* c = spd_contents; *c != 0; c = find_next_line(c))
+	{
+		//Increment number of samples
+		++number_of_samples;
+	}
+
+	//printf("Number of samples = %d\n", number_of_samples);
+	spd.number_of_samples = number_of_samples;
+
+	char* c = spd_contents;
+	for(int i = 0; i < number_of_samples; ++i)
+	{
+		if(i == 0)
+		{
+			spd.start_wavelength = (long double) atof(c);
+		}
+		else if(i == number_of_samples - 1)
+		{
+			spd.end_wavelength = (long double) atof(c);
+		}
+
+		c = find_next_number(find_next_character(c, ','));
+		spd.samples[i] = (long double) atof(c);
+
+		c = find_next_line(c);
+	}
+
+	dealloc(spd_contents);
+
+	return spd;
+}
+
+void load_colour_matching_functions()
+{
+	colour_matching_functions[0] = load_spd("cmf_x.csv");
+	colour_matching_functions[1] = load_spd("cmf_y.csv");
+	colour_matching_functions[2] = load_spd("cmf_z.csv");
+}
+
+void load_d65_illuminant()
+{
+	reference_white = load_spd("d65.csv");
+	//normalise(reference_white);
+	for(int i = 0; i < reference_white.number_of_samples; ++i) reference_white.samples[i] /= 100.0L;
+}
+
+void load_e_illuminant()
+{
+	long double wavelength_interval = 5.0L;
+	reference_white.start_wavelength = 380.0L;
+	reference_white.end_wavelength = 720.0L;
+	reference_white.number_of_samples = ((reference_white.end_wavelength-reference_white.start_wavelength)/ wavelength_interval) + 1;
+	for(int i = 0; i < reference_white.number_of_samples; ++i) reference_white.samples[i] = 1.0L;
+}
+
+void load_rgb_to_spd_functions()
+{
+	white_rgb_to_spd = load_spd("white_rgb_to_spd.csv");
+	cyan_rgb_to_spd = load_spd("cyan_rgb_to_spd.csv");
+	magenta_rgb_to_spd = load_spd("magenta_rgb_to_spd.csv");
+	yellow_rgb_to_spd = load_spd("yellow_rgb_to_spd.csv");
+	red_rgb_to_spd = load_spd("red_rgb_to_spd.csv");
+	green_rgb_to_spd = load_spd("green_rgb_to_spd.csv");
+	blue_rgb_to_spd = load_spd("blue_rgb_to_spd.csv");
+}
+
+void load_colour_data()
+{
+	load_rgb_to_spd_functions();
+	load_e_illuminant();
+	//load_d65_illuminant();
+	load_colour_matching_functions();
+}
+
+Vec3 spectrum_to_xyz(Spectrum spd)
+{
+	//SPD -> XYZ
+	Spectrum spd_xyz[3] = {};
+	spd_xyz[0] = spd * colour_matching_functions[0];
+	spd_xyz[1] = spd * colour_matching_functions[1];
+	spd_xyz[2] = spd * colour_matching_functions[2];
+
+	Vec3 xyz = {};
+	long double l = (spd.end_wavelength - spd.start_wavelength)/(long double)(spd.number_of_samples);
+	for(int i = 0; i < 3; ++i)
+	{
+		for(int j = 0; j < spd.number_of_samples; ++j)
+		{
+			xyz[i] += (double)spd_xyz[i].samples[j];
+		}
+	}
+	xyz *= l;
+	
+	return xyz;
+}
+
+RGB64 gamma_linear_rgb(RGB64 linear_rgb)
+{
+	RGB64 rgb = {};
+	for(int i = 0; i < 3; ++i)
+	{
+		rgb[i] = (linear_rgb[i] <= 0.0031308) ? 12.92 * linear_rgb[i] : (1.055 * pow(linear_rgb[i], 1.0/2.4)) - 0.055;
+	}
+	return rgb;
+}
+
+RGB64 inverse_gamma_rgb(RGB64 rgb)
+{
+	RGB64 linear_rgb = {};
+	for(int i = 0; i < 3; ++i)
+	{
+		linear_rgb[i] = (rgb[i] <= 0.04045) ? rgb[i]/12.92 : pow(((rgb[i] + 0.055)/1.055), 2.4);
+	}
+	return linear_rgb;
+}
+
+RGB64 spectrum_to_RGB64(Spectrum spd)
+{
+	Vec3 xyz = spectrum_to_xyz(spd);
+	Vec3 white_xyz = spectrum_to_xyz(reference_white);
+
+	xyz /= white_xyz.y;
+
+	//XYZ -> RGB
+	RGB64 linear_rgb = {};
+	linear_rgb.R = 3.24097 * xyz.x - 1.53738 * xyz.y - 0.49831 * xyz.z;
+	linear_rgb.G = -0.96934 * xyz.x + 1.87596 * xyz.y + 0.04156 * xyz.z;
+	linear_rgb.B = 0.05563 * xyz.x - 0.20398 * xyz.y + 1.05697 * xyz.z;
+
+	RGB64 rgb = gamma_linear_rgb(linear_rgb);
+	return rgb;
+}
+
+Spectrum RGB64_to_spectrum(RGB64 rgb)
+{
+	Spectrum spd = {};
+	spd.start_wavelength = white_rgb_to_spd.start_wavelength;
+	spd.end_wavelength = white_rgb_to_spd.end_wavelength;
+	spd.number_of_samples = white_rgb_to_spd.number_of_samples;
+	if(rgb.R <= rgb.G && rgb.R <= rgb.B)
+	{
+		spd += rgb.R * white_rgb_to_spd;
+		if(rgb.G <= rgb.B)
+		{
+			spd += (rgb.G - rgb.R)*cyan_rgb_to_spd;
+			spd += (rgb.B - rgb.G)*blue_rgb_to_spd;
+		}
+		else
+		{
+			spd += (rgb.B - rgb.R)*cyan_rgb_to_spd;
+			spd += (rgb.G - rgb.B)*green_rgb_to_spd;
+		}
+	}
+	else if(rgb.G <= rgb.R && rgb.G <= rgb.B)
+	{
+		spd += rgb.G * white_rgb_to_spd;
+		if(rgb.R <= rgb.B)
+		{
+			spd += (rgb.R - rgb.G)*magenta_rgb_to_spd;
+			spd += (rgb.B - rgb.R)*blue_rgb_to_spd;
+		}
+		else
+		{
+			spd += (rgb.B - rgb.G)*magenta_rgb_to_spd;
+			spd += (rgb.R - rgb.B)*red_rgb_to_spd;
+		}
+	}
+	else
+	{
+		spd += rgb.B * white_rgb_to_spd;
+		if(rgb.R <= rgb.G)
+		{
+			spd += (rgb.R - rgb.B)*yellow_rgb_to_spd;
+			spd += (rgb.G - rgb.R)*green_rgb_to_spd;
+		}
+		else
+		{
+			spd += (rgb.G - rgb.B)*yellow_rgb_to_spd;
+			spd += (rgb.R - rgb.G)*red_rgb_to_spd;
+		}
+	}
+
+	return spd;
+}
--- src/Colour.h
+++ src/Colour.h
@@ -1,4 +1,5 @@
 #include "Maths.h"
+#include "Platform.h"
 
 struct RGB8
 {
@@ -10,7 +11,7 @@ typedef Vec3 RGB64;
 
 RGB8 rgb64_to_rgb8(RGB64);
 
-#define SPECTRUM_SAMPLE_MAX 1024
+#define SPECTRUM_SAMPLE_MAX 512
 
 struct Spectrum
 {
@@ -25,4 +26,11 @@ Spectrum operator*(double, Spectrum);
 Spectrum operator/(Spectrum, double);
 void operator+=(Spectrum&, Spectrum);
 void normalise(Spectrum& spd);
-Spectrum generate_black_body_spd(double temperature, double start_wavelength = 300.0, double end_wavelength = 780.0, double sample_interval = 5.0);
+
+Spectrum generate_black_body_spd(double temperature, double start_wavelength = 380.0, double end_wavelength = 720.0, double sample_interval = 5.0);
+
+RGB64 spectrum_to_RGB64(Spectrum);
+Spectrum RGB64_to_spectrum(RGB64);
+
+Spectrum load_spd(const char* spd_path);
+void load_colour_data();
