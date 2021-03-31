cdca40aff2422c2c46f273ca0366297ea75de87c
sc15i2gp <sc15i2gp@leeds.ac.uk>
2021-03-31 03:54:37 +0100

    Output files are now deleted before compilation.

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 IF NOT EXIST src (mkdir src)
 IF NOT EXIST output (mkdir output)
 IF NOT EXIST bin (mkdir bin)
+del *.obj bin\*.ilk bin\*.exe bin\*.pdb
 g++ src\main.cpp -lgdi32 -o bin\raytrace.exe
-cl /DDEBUG_BUILD /Zi src\main.cpp /Fe:bin\debug_raytrace.exe user32.lib gdi32.lib
+cl /DDEBUG_BUILD /F 4194304 /Zi src\main.cpp /Fe:bin\debug_raytrace.exe user32.lib gdi32.lib
