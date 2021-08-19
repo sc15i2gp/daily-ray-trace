49a32ff0901f26f4a746339d2cd3353dca138865
sc15i2gp <sc15i2gp@leeds.ac.uk>
2021-08-19 13:55:04 +0100

    Changed march g++ flag.

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ IF NOT EXIST src (mkdir src)
 IF NOT EXIST output (mkdir output)
 IF NOT EXIST bin (mkdir bin)
 del *.obj bin\*.ilk bin\*.exe bin\*.pdb
-g++ src\main.cpp -static-libgcc -static-libstdc++ -lgdi32 -O3 -o bin\raytrace.exe
+g++ src\main.cpp -static-libgcc -static-libstdc++ -lgdi32 -march=native -O3 -o bin\raytrace.exe
 cl /DDEBUG_BUILD /F 4194304 /Zi /MD src\main.cpp /Fe:bin\debug_raytrace.exe user32.lib gdi32.lib /link /debug:full
 cl /F 4194304 /Zi /MD /O2it src\main.cpp /Fe:bin\profile_raytrace.exe user32.lib gdi32.lib /link /debug:full
