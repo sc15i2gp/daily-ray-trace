d952292aeb9f01942e1c5f2962bf09a84093089e
sc15i2gp <sc15i2gp@leeds.ac.uk>
2021-08-30 00:48:32 +0100

    Changed stack size flag on msvc.

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ IF NOT EXIST output (mkdir output)
 IF NOT EXIST bin (mkdir bin)
 del *.obj bin\*.ilk bin\*.exe bin\*.pdb
 g++ src\main.cpp -static-libgcc -static-libstdc++ -lgdi32 -march=native -O3 -o bin\raytrace.exe
-cl /DDEBUG_BUILD /F 4194304 /Zi /MD src\main.cpp /Fe:bin\debug_raytrace.exe user32.lib gdi32.lib /link /debug:full
-cl /F 4194304 /Zi /MD /O2it src\main.cpp /Fe:bin\profile_raytrace.exe user32.lib gdi32.lib /link /debug:full
+cl /DDEBUG_BUILD /Zi /MD src\main.cpp /Fe:bin\debug_raytrace.exe user32.lib gdi32.lib /link /debug:full
+cl /Zi /MD /O2it src\main.cpp /Fe:bin\profile_raytrace.exe user32.lib gdi32.lib /link /debug:full
