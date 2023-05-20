f661c0be2ff1fde28be32989666b8bb5572481f6
sc15i2gp <isaac.perry97@gmail.com>
2023-05-20 16:39:33 +0100

    Compiling in C. Debugging available.

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ IF NOT EXIST src (mkdir src)
 IF NOT EXIST output (mkdir output)
 IF NOT EXIST bin (mkdir bin)
 del *.obj bin\*.ilk bin\*.exe bin\*.pdb
-g++ src\main.cpp -static-libgcc -static-libstdc++ -lgdi32 -march=skylake -O3 -ffast-math -fno-trapping-math -Isrc -o bin\raytrace.exe
-cl /DDEBUG_BUILD /Zi /MD src\main.cpp /Fe:bin\debug_raytrace.exe user32.lib gdi32.lib /link /debug:full
-cl /Zi /MD /O2 src\main.cpp /Qvec-report:0 /arch:AVX2 /Fe:bin\profile_raytrace.exe user32.lib gdi32.lib /link /debug:full
+gcc src\main.c -Isrc -o bin\raytrace.exe
+cl /DDEBUG_BUILD /Zi /TC src\main.c /Fe:bin\debug_raytrace.exe /link /debug:full
+cl /Zi /TC src\main.c /Fe:bin\profile_raytrace.exe /link /debug:full
