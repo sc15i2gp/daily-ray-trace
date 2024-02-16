cac06554c879178810ab00d34dfba31d2a5d2e0d
sc15i2gp <isaac.perry97@gmail.com>
2024-02-16 18:31:03 +0000

    Changed optimisation level for GCC build.

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ IF NOT EXIST src (mkdir src)
 IF NOT EXIST output (mkdir output)
 IF NOT EXIST bin (mkdir bin)
 del *.obj bin\*.ilk bin\*.exe bin\*.pdb
-gcc src\win32_main.c -Isrc -o bin\raytrace.exe
+gcc src\win32_main.c -Isrc -O3 -o bin\raytrace.exe
 cl /DDEBUG_BUILD /Zi /TC src\win32_main.c /Fe:bin\debug_raytrace.exe /link /debug:full
 cl /DDEBUG_BUILD /Zi /TC src\test.c /Fe:bin\test_raytrace.exe /link /debug:full
 cl /Zi /TC src\win32_main.c /Fe:bin\profile_raytrace.exe /link /debug:full
